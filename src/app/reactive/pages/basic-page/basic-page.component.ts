0b7468b6e6297c035eff18c5151e6761fac38811
Walter Reiner <wreiner@inti.gob.ar>
2024-12-12 14:36:08 -0300

    ->ACT: funcionando basic

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,11 +1,56 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { retry } from 'rxjs';
 
 @Component({
   standalone: false,
-  
   templateUrl: './basic-page.component.html',
   styles: ``
 })
 export class BasicPageComponent {
+
+  public mainForm: FormGroup;
+
+  constructor( private formbuilder: FormBuilder ){
+    this.mainForm =  this.formbuilder.group({
+      name:['',[Validators.required, Validators.minLength(3)]],
+      price:[0, [Validators.required, Validators.min(0)]],
+      inStorage:[0, [Validators.required, Validators.min(0)]],
+    })
+  }
+
+  isValidField(field: string){
+    return this.mainForm.controls[field].errors
+    && this.mainForm.controls[field].touched;
+  }
+
+  getFieldError(field: string): string | null {
+    if(!this.mainForm.controls[field]) return null;
+
+    const error = this.mainForm.controls[field].errors || {};;
+
+    for ( const key of Object.keys(error) ){
+
+      switch (key){
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Mínimo ${error['minlength'].requiredLenght} caracteres.`;
+      }
+    }
+
+    return null
+  }
+
+  onSave():void {
+    if (this.mainForm.invalid) {
+      this.mainForm.markAllAsTouched();
+      return;
+    };
+
+    console.log(this.mainForm.value);
+
+    this.mainForm.reset({price :0, inStorage:0 });
+  }
 
 }
--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
 import { ReactiveRoutingModule } from './reactive-routing.module';
 import { BasicPageComponent } from './pages/basic-page/basic-page.component';
 import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
 import { SwitchedPageComponent } from './pages/switched-page/switched-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -16,7 +18,8 @@ import { SwitchedPageComponent } from './pages/switched-page/switched-page.compo
   ],
   imports: [
     CommonModule,
-    ReactiveRoutingModule
+    ReactiveRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ReactiveModule { }
